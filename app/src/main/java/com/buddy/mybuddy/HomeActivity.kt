9a3ed181ff08d1a9f088e85c8d850eb2b82d8254
sujay95 <sujay95@bitbucket.org>
2022-06-25 21:21:21 +0530

    chkout chk

--- app/src/main/java/com/buddy/mybuddy/HomeActivity.kt
+++ app/src/main/java/com/buddy/mybuddy/HomeActivity.kt
@@ -8,24 +8,15 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        var btnSend=findViewById<Button>(R.id.btnSend)
-        var btnAdd=findViewById<Button>(R.id.btnAdd)
+        var btnSend = findViewById<Button>(R.id.btnSend)
+        var btnAdd = findViewById<Button>(R.id.btnAdd)
         btnSend.setOnClickListener {
             // send alert to saved 10 contacts
-
 
 
         }
         btnAdd.setOnClickListener {
             // send alert to saved 10 contacts
-
-show_contacts()
-
         }
-
-    }
-    fun show_contacts()
-    {
-
     }
 }
